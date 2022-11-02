28e00c33544a63e221acff8652978733a831c0b5
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2022-11-02 17:04:58 +1100

    Add github comments

---
+++ content/components/utterances/index.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+const UtterancesComments = ({ repo, theme, issueTerm }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    const config = {
+      src: 'https://utteranc.es/client.js',
+      repo: repo,
+      'issue-term': issueTerm,
+      theme: theme,
+      crossOrigin: 'anonymous',
+      defer: true
+    };
+
+    Object.entries(config).forEach(([key, value]) => {
+      script.setAttribute(key, value);
+    });
+
+    setTimeout(() => {
+      ref.current.append(script);
+    }, 300);
+  }, [repo, theme, issueTerm]);
+
+  return <div ref={ref} />;
+};
+
+UtterancesComments.defaultProps = {
+  theme: 'github-light',
+  issueTerm: 'pathname',
+  repo: 'tombrereton/pedlar'
+};
+
+UtterancesComments.propTypes = {
+  /** The name of the public repository to open issues  */
+  repo: PropTypes.string.isRequired,
+  /** The type of Theme to use */
+  theme: PropTypes.oneOf([
+    'github-light',
+    'github-dark',
+    'preferred-color-scheme',
+    'github-dark-orange',
+    'icy-dark',
+    'dark-blue',
+    'photon-dark',
+    'boxy-light',
+    'gruvbox-dark'
+  ]),
+  /** The property to use as the issue title */
+  issueTerm: PropTypes.string
+};
+
+export default UtterancesComments;
